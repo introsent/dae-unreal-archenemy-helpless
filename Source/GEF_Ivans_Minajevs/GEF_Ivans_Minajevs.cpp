7fb87a13185bbe049e6de22f5fa62aca0b5d9a6e
introsent <ivans.minajevs@gmail.com>
2024-11-30 17:07:29 +0100

    feat: create first commit for DAE Unreal project

---
+++ Source/GEF_Ivans_Minajevs/GEF_Ivans_Minajevs.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GEF_Ivans_Minajevs.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GEF_Ivans_Minajevs, "GEF_Ivans_Minajevs" );
